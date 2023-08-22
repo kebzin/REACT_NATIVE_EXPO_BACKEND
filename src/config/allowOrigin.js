b791eec0e2c9e448dcf766c0326ae5c41b8830b6
kebzin <honorablewaiga@gmail.com>
2023-08-22 09:15:48 +0000

    allow origin change

--- src/config/allowOrigin.js
+++ src/config/allowOrigin.js
@@ -1,5 +1,5 @@
 // This file list the allow the origin that alre allow to acces our api
 
-const allowOrigin = ["http://192.168.132.44:19000"];
+const allowOrigin = ["http:192.168.115.44:19000"];
 
 module.exports = allowOrigin;
